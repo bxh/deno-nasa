17fac6d21532b9cc242a7327445b96ff84a027f4
Bangxin Hu <hu.bangxin@gmail.com>
2020-12-31 12:53:34 -0500

    Add basic error handling.

--- mod.ts
+++ mod.ts
@@ -17,11 +17,24 @@ await log.setup({
     }
 });
 
+app.addEventListener("error", (event) => {
+    log.error(event.error);
+});
+
+app.use(async (ctx, next) => {
+    try {
+        await next();
+    } catch (err) {
+        ctx.response.body = "Internal server error";
+        throw err;
+    }
+});
+
 app.use(async (ctx, next) => {
     await next();
     const time = ctx.response.headers.get("X-Response-Time");
     log.info(`${ctx.request.method} ${ctx.request.url}: ${time}`);
-})
+});
 
 app.use(async (ctx, next) => {
     const start = Date.now();
